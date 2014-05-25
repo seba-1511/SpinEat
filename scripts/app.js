09493d79ad190f6bb54c6ede7156d77ce9900725
seba-1511 <seba-1511@hotmail.com>
2014-05-25 15:25:13 +0200

    Voilà.

--- scripts/app.js
+++ scripts/app.js
@@ -82,3 +82,24 @@ var startCooking = function() {
     jQuery('#first-image')
         .click();
 };
+
+
+function getUrlVars() {
+    var vars = [],
+        hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') +
+        1)
+        .split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+var test = function() {
+    if (!getUrlVars()["me"] == '1') {
+        window.location.href = document.URL + '?me=1';
+    }
+}
